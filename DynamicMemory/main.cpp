a3cc994448509f0eaa1810f9fe3c9561a4d12741
Atixson <Atixson37@gmail.com>
2023-04-07 22:19:44 +0300

    recoding

--- DynamicMemory/main.cpp
+++ DynamicMemory/main.cpp
@@ -5,6 +5,13 @@ void FillRand(int arr[], const int n);
 void Print(int arr[], const int n);
 
 int* push_back(int arr[], int& n, int value);
+int* push_front(int arr[], int& n, int value);
+
+int* pop_back(int arr[], int& n);
+int* pop_front(int arr[], int& n);
+
+int* insert(int arr[], int& n, int value, int index_insert);
+int* erase(int arr[], int& n, int index_erase);
 
 void main()
 {
@@ -20,7 +27,13 @@ void main()
 	cout << "Введите добавляемое значение: "; cin >> value;
 
 	arr = push_back(arr, n, value);
+	arr = push_front(arr, n, value);
+	Print(arr, n);
 
+	arr = pop_back(arr, n);
+	Print(arr, n);
+
+	arr = pop_front(arr, n);
 	Print(arr, n);
 
 	delete[] arr;
@@ -46,7 +59,7 @@ void Print(int arr[], const int n)
 
 int* push_back(int arr[], int& n, int value)
 {
-	//1) Создаем беферный массив нужного размера:
+	//1) Создаем буферный массив нужного размера:
 	int* buffer = new int[n + 1];
 
 	//2) Копируем все значения из исходного массива в буферный:
@@ -68,5 +81,67 @@ int* push_back(int arr[], int& n, int value)
 	n++;
 
 	//6) Возвращаем новый массив:
+	return arr;
+}
+int* push_front(int arr[], int& n, int value)
+{
+	int* buffer = new int[n + 1];
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i + 1] = arr[i];
+	}
+	delete[] arr;
+	arr = buffer;
+	arr[0] = value;
+	n++;
+	return buffer;
+}
+
+int* pop_back(int arr[], int& n)
+{
+	int* buffer = new int[--n];
+	for (int i = 0; i < n; i++)buffer[i] = arr[i];
+	delete[] arr;
+	return buffer;
+}
+int* pop_front(int arr[], int& n)
+{
+	int* buffer = new int[--n];
+	for (int i = 0; i < n; i++)buffer[i] = arr[i+1];
+	delete[] arr;
+	return buffer;
+}
+
+int* insert(int arr[], int& n, int value, int index_insert)
+{
+	int* buffer = new int[n + 1];
+	for (int i = 0; i < n; i++)
+	{
+		if (i < index_insert)
+		{
+			buffer[i] = arr[i];
+		}
+		buffer[i + 1] = arr[i];
+	}
+	delete[] arr;
+	arr = buffer;
+	buffer[index_insert - 1] = value;
+	n++;
+	return arr;
+}
+int* erase(int arr[], int& n, int index_erase)
+{
+	int* buffer = new int[n - 1];
+	for (int i = 0; i < n; i++)
+	{
+		if (i < index_erase)
+		{
+			buffer[i] = arr[i];
+		}
+		buffer[i + 1] = arr[i + 2];
+	}
+	delete[] arr;
+	arr = buffer;
+	n--;
 	return arr;
 }
---
+++ Split/main.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+void FillRand(int arr[], const int n);
+void Print(int arr[], const int n);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int n;
+	cout << "¬ведите количество элементов массива : "; cin >> n;
+	int* arr = new int[n];
+	FillRand(arr, n);
+	Print(arr, n);
+	int even = 0;
+	int	odd = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] % 2 == 0)
+		{
+			even++;
+		}
+		else odd++;
+	}
+	int* even_arr = new int[even];
+	int* odd_arr = new int[odd];
+	for (int i = 0, j = 0, k = 0; i < n; i++)
+	{
+		if (arr[i] % 2 == 0)
+		{
+			even_arr[j++] = arr[i];
+		}
+		else odd_arr[k++] = arr[i];
+	}
+	Print(even_arr, even);
+	Print(odd_arr, odd);
+	delete[] even_arr;
+	delete[] odd_arr;
+}
+void FillRand(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 100;
+	}
+}
+void Print(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
